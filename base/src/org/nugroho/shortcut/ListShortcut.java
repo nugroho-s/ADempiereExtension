82b028289f6f40f8290aa59fbbc8050b84cdb441
Nugroho Satriyanto <massatriya@gmail.com>
2017-02-07 16:32:53 +0700

    tambah sortcut

---
+++ base/src/org/nugroho/shortcut/ListShortcut.java
@@ -0,0 +1,24 @@
+package org.nugroho.shortcut;
+
+public class ListShortcut {
+	public static Shortcut PrintScreen = new Shortcut("Print Screen", "PrintScreen");
+	public static Shortcut ScreenShot = new Shortcut("Screen Shot", "shift+PrintScreen");
+	public static Shortcut LogOut = new Shortcut("Log Out", "Alt+Shift+L");
+	public static Shortcut Exit = new Shortcut("Exit Application", "Alt+Shift+X");
+	public static Shortcut ProductInfo = new Shortcut("Product Info","Alt+I");
+    public static Shortcut BusinessPartnerInfo = new Shortcut("Business Partner Info","Ctrl+Alt+I");
+    public static Shortcut AccountInfo = new Shortcut("Account Info","Ctrl+Alt+I");
+    public static Shortcut Report = new Shortcut("Report","F11");
+    public static Shortcut Print = new Shortcut("Print","F12");
+    public static Shortcut PrintPreview = new Shortcut("Print Preview","Alt+Shift+P");
+    public static Shortcut ExitWindow = new Shortcut("Exit Window","Alt+X");
+    public static Shortcut NewRec = new Shortcut("New Record","F2");
+    public static Shortcut Save = new Shortcut("Save Changes","F4");
+    public static Shortcut CopyRec = new Shortcut("Copy Record","Shift+F2");
+    public static Shortcut Delete = new Shortcut("Delete Record","F3");
+    public static Shortcut DeleteSelected = new Shortcut("Delete Selected","Ctrl+D");
+    public static Shortcut Undo = new Shortcut("Undo","Esc");
+    public static Shortcut ReQuery = new Shortcut("ReQuery","F5");
+    public static Shortcut Lookup = new Shortcut("Lookup","F6");
+    public static Shortcut Attachment = new Shortcut("Attachment","F7");
+}
---
+++ base/src/org/nugroho/shortcut/Shortcut.java
@@ -0,0 +1,23 @@
+package org.nugroho.shortcut;
+
+public class Shortcut {
+	private String name;
+	private String shortcut;
+	public Shortcut(String name, String shortcut) {
+		this.name = name;
+		this.shortcut = shortcut;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShortcut() {
+		return shortcut;
+	}
+	public void setShortcut(String shortcut) {
+		this.shortcut = shortcut;
+	}
+	
+}
